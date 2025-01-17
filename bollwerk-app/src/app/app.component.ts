9dd9c7b2a07a98829b33d744d0d6d4b8753b5e97
Davide Rossi <davide.rossi39@studio.unibo.it>
2025-01-17 12:32:48 +0100

    started building page

--- bollwerk-app/src/app/app.component.ts
+++ bollwerk-app/src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule],
+  imports: [
+    MatSidenavModule,
+    MatGridListModule,
+    MatCardModule,
+    MatSelectModule,
+    CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'bollwerk-app';
+  public appTypes = [
+    'Chat',
+    'Call Analyzer',
+    'Compare documents',
+    'Due Dilligence',
+    'KYC Profile',
+    'Presentation Wizard',
+    'Researcher',
+    'Translator'
+  ]
+
 }
