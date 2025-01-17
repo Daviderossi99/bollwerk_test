8c728ab9a3e6e2f6eb71c68e5f33c73c0e046810
Davide Rossi <davide.rossi39@studio.unibo.it>
2025-01-17 10:53:27 +0100

    updated to angular 19

--- bollwerk-app/src/app/app.component.spec.ts
+++ bollwerk-app/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bollwerk-app'`, () => {
+  it(`should have the 'bollwerk-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('bollwerk-app');
@@ -26,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bollwerk-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bollwerk-app');
   });
 });
